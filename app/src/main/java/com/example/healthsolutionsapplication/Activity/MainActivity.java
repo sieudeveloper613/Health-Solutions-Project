777728f47f6c106a12f289166996fd03b80824b7
Hieu Trung <hieutrung22071999@gmail.com>
2022-03-07 14:37:39 +0700

    no

--- app/src/main/java/com/example/healthsolutionsapplication/Activity/MainActivity.java
+++ app/src/main/java/com/example/healthsolutionsapplication/Activity/MainActivity.java
@@ -1,16 +1,30 @@
 package com.example.healthsolutionsapplication.Activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.healthsolutionsapplication.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView btn_discover;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_discover=findViewById(R.id.btn_discover);
+
+        btn_discover.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
 }
