460c0091dbc5787b92e0ab0c7d134f1ac889751a
TonyThuong <hahoangthuong02@gmail.com>
2022-03-11 18:21:12 +0700

    Thuong sua Layout, them Model Product, Admin, Cart

--- app/src/main/java/com/example/healthsolutionsapplication/Activity/sanPhamDaMuaActivity.java
+++ app/src/main/java/com/example/healthsolutionsapplication/Activity/sanPhamDaMuaActivity.java
@@ -1,9 +1,13 @@
 package com.example.healthsolutionsapplication.Activity;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import com.example.healthsolutionsapplication.Adapter.sanPhamDaMuaAdapter;
@@ -22,6 +26,7 @@ public class sanPhamDaMuaActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_san_pham_da_mua);
         recyclerView = findViewById(R.id.rv_purchasedProduct);
+        customActionBar();
         // lay du lieu
         list.add(new sanPhamDaMuaModel(R.mipmap.ic_launcher, "Máy đo nhịp tim cao cấp", "Còn Hàng","Chi Tiết"));
         list.add(new sanPhamDaMuaModel(R.mipmap.ic_launcher, "Máy đo nhịp tim cao cấp 2", "Hết Hàng","Chi Tiết"));
@@ -30,5 +35,14 @@ public class sanPhamDaMuaActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(linearLayoutManager);
         sanPhamDaMuaAdapter adapter = new sanPhamDaMuaAdapter(list,this);
         recyclerView.setAdapter(adapter);
+
+    }
+    private void customActionBar(){
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Địa Chỉ Nhận Hàng");
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#0088FF")));
+        Drawable drawable= getResources().getDrawable(R.drawable.ic_arrow_back_24px);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeAsUpIndicator(drawable);
     }
 }
