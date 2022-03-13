9985d860ecd97b7947af5bbdd85191d07324db36
TonyThuong <hahoangthuong02@gmail.com>
2022-03-13 10:57:21 +0700

    Thuong sua loi icon

--- app/src/main/java/com/example/healthsolutionsapplication/Activity/RegisterActivity.java
+++ app/src/main/java/com/example/healthsolutionsapplication/Activity/RegisterActivity.java
@@ -1,7 +1,9 @@
 package com.example.healthsolutionsapplication.Activity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -20,7 +22,20 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         changeStatusBarColor();
+
+        tvReturn = findViewById(R.id.tv_return);
+
+        tvReturn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        });
     }
+
+
 
     private void changeStatusBarColor(){
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
